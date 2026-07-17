a8b1180f5f07c426b888275994a281cb09ce0b71
Tanvir <tanvir14012@gmail.com>
2026-07-18 03:32:59 +0600

    Fix The entity type 'LocalizedText' requires a primary key to be defined issue

---
+++ Services/Procurement/src/Procurement.Infrastructure/Persistence/Configurations/SupplierEntityConfiguration.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Procurement.Domain.Suppliers;
+
+namespace Procurement.Infrastructure.Persistence.Configurations;
+
+internal sealed class SupplierEntityConfiguration : IEntityTypeConfiguration<Supplier>
+{
+    public void Configure(EntityTypeBuilder<Supplier> builder)
+    {
+        builder.ToTable("suppliers");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.ContactEmail)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.Property(x => x.ContactPhone)
+            .HasMaxLength(32);
+
+        builder.Property(x => x.Address)
+            .HasMaxLength(512);
+
+        builder.Property(x => x.IsActive)
+            .IsRequired();
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+
+        builder.HasIndex(x => x.ContactEmail);
+    }
+}
